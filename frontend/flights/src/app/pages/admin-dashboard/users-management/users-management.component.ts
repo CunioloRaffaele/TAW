220194c04bf40716da22125814e74ce62ba160f6
GitBariAl <abarison02@gmail.com>
2025-06-05 10:57:40 +0200

    little flex from ng components (admin, user  management)

--- frontend/flights/src/app/pages/admin-dashboard/users-management/users-management.component.ts
+++ frontend/flights/src/app/pages/admin-dashboard/users-management/users-management.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { UserCardComponent } from '../../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-users-management',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, UserCardComponent],
   templateUrl: './users-management.component.html',
   styleUrls: ['./users-management.component.css']
 })
