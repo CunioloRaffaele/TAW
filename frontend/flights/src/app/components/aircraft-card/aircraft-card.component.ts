565ff2de0df91a12e1fac2976e51ad922ece8170
Alberto <abarison02@gmail.com>
2025-06-18 14:39:14 +0200

    remove of usless read.me

---
+++ frontend/flights/src/app/components/aircraft-card/aircraft-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aircraft-card',
+  imports: [],
+  templateUrl: './aircraft-card.component.html',
+  styleUrl: './aircraft-card.component.css'
+})
+export class AircraftCardComponent {
+
+}
