23372de30770cd4f5759216b89c18673f0532342
Cuniolo Raffaele <raffaelecuniolo@gmail.com>
2025-06-18 11:11:00 +0200

    Prisma update incremental

--- backend/utils/prisma.js
+++ backend/utils/prisma.js
@@ -1,3 +1,5 @@
 const { PrismaClient } = require('../prisma/generated/prisma');
+// migrated prisma to automate database schema changes
+// const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 module.exports = prisma;
