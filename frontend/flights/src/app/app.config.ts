1f9bd528493797a6e8768b5d972429907ac65938
Cuniolo Raffaele <raffaelecuniolo@gmail.com>
2025-06-19 13:20:25 +0200

    JWTs expiration alert / redirect
    Now users with expired JWTs gets redirected to login page

--- frontend/flights/src/app/app.config.ts
+++ frontend/flights/src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ServerErrorInterceptor } from './interceptors/server-error.interceptor';
+import { JwtExpiryInterceptor } from './interceptors/jwt-expiration.interceptor';
 
 export const appConfig = {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServerErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtExpiryInterceptor,
+      multi: true
+    }
   ]
 };
