c7433017cca88c850f27c28ac6d9dfac0e7e46f4
Cuniolo Raffaele <raffaelecuniolo@gmail.com>
2025-06-19 16:04:31 +0200

    fix map + jwt

--- frontend/flights/src/app/app.config.ts
+++ frontend/flights/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { ServerErrorInterceptor } from './interceptors/server-error.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ServerErrorInterceptor } from './interceptors/server-error.interceptor';
+import { JwtExpiryInterceptor } from './interceptors/jwt-expiration.interceptor';
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
-    { provide: HTTP_INTERCEPTORS, useClass: ServerErrorInterceptor, multi: true },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServerErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtExpiryInterceptor,
+      multi: true
+    }
   ]
 };
--- frontend/flights/src/app/components/map/map.component.ts
+++ frontend/flights/src/app/components/map/map.component.ts
@@ -1,79 +1,64 @@
 import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import * as L from 'leaflet';
-
-// Fix icone marker Leaflet in Angular
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
-
-L.Marker.prototype.options.icon = L.icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
-});
 
 @Component({
   selector: 'app-map',
-  standalone: true,
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
+
 export class LeafletMapComponent implements AfterViewInit {
   private map!: L.Map;
-  private allAirports: any[] = [];
-  private allMarkers: L.Marker[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngAfterViewInit(): void {
-    this.map = L.map('map', {
-      center: [45, 9],
-      zoom: 4,
-      zoomControl: true
-    });
+    this.map = L.map('map').setView([45.4642, 9.19], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: '© OpenStreetMap'
+      attribution: '© OpenStreetMap contributors'
     }).addTo(this.map);
 
     this.loadAirports();
   }
 
   private loadAirports(): void {
-    this.http.get<any>(`${environment.apiUrl}/api/navigate/airports/`).subscribe({
+    const url = `${environment.apiUrl}/api/navigate/airports/`;
+
+    // Set map bounds
+    const southWest = L.latLng(-60, -170);
+    const northEast = L.latLng(85, 190);
+    const bounds = L.latLngBounds(southWest, northEast);
+
+    this.map.setMaxBounds(bounds);
+    this.map.on('drag', () => {
+      this.map.panInsideBounds(bounds, { animate: false });
+    });
+    this.map.options.minZoom = 2;
+    this.http.get<any>(url).subscribe({
       next: (response) => {
-        this.allAirports = response.airports || [];
-        this.showAllAirports();
+        if (response.airports) {
+          response.airports.forEach((airport: any) => {
+            if (airport.lat && airport.lan) {
+              const localTime = new Date().toLocaleTimeString('en-US', {
+                timeZone: airport.time_zone,
+                hour: '2-digit',
+                minute: '2-digit',
+                day: '2-digit',
+                month: '2-digit',
+                hour12: false
+              });
+              L.marker([airport.lat, airport.lan])
+                .addTo(this.map)
+                .bindPopup(`<b>${airport.name}</b><br>${airport.city}, ${airport.country}<br> ${airport.time_zone}<br>Local Time: ${localTime}`);
+            }
+          });
+        }
       },
       error: (err) => {
-        console.error('Errore caricamento aeroporti:', err);
+        console.error('Failed to load airports:', err);
       }
     });
-  }
-
-  private showAllAirports(): void {
-    this.allMarkers.forEach(m => this.map.removeLayer(m));
-    this.allMarkers = [];
-    this.allAirports.forEach(airport => {
-      const lat = airport.lat;
-      const lng = airport.lng !== undefined ? airport.lng : airport.lan;
-      if (lat && lng) {
-        const marker = L.marker([lat, lng])
-          .addTo(this.map)
-          .bindPopup(`<b>${airport.name}</b><br>${airport.city}, ${airport.country}`);
-        this.allMarkers.push(marker);
-      }
-    });
-    if (this.allMarkers.length > 0) {
-      const group = L.featureGroup(this.allMarkers);
-      this.map.fitBounds(group.getBounds().pad(0.2));
-    }
   }
 }
